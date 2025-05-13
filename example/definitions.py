12292108ec5d7bfb385e308fb6e5734c1558a2df
Marcelo Alvarez <marceloalvarez77777@gmail.com>
2025-05-13 02:31:38 -0300

    Primer version - Proyecto Toyota Corolla en Dagster

--- example/definitions.py
+++ example/definitions.py
@@ -1,26 +1,13 @@
-from dagster import (
-    Definitions,
-    ScheduleDefinition,
-    define_asset_job,
-    load_assets_from_modules,
-)
-
-from example import assets  # noqa: TID252
-
-all_assets = load_assets_from_modules([assets])
-
-all_sync_jobs = define_asset_job(
-    name="all_sync_jobs",
-    selection=all_assets,
-)
-
-every_day = ScheduleDefinition(
-    job=all_sync_jobs,
-    cron_schedule="0 0 * * *",  # Every day at midnight
-)
+from dagster import Definitions
+from . import assets
 
 defs = Definitions(
-    assets=all_assets,
-    jobs=[all_sync_jobs],
-    schedules=[every_day],
+    assets=[
+        assets.load_data,
+        assets.eda,
+        assets.prepare_data,
+        assets.train_model,
+        assets.evaluate_model,
+        assets.analyze_residuals,
+    ]
 )
